6223fac9b392fb1e0ef4896da4fbecfcc1decc3e
Rich Edwards <richedw@gmail.com>
2013-12-04 15:58:14 -0600

    Added dynamic segments.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,44 +1,75 @@
 class VideosController < ApplicationController
   
   def show_all
-    @params = params
-  end
-
-  def show
-    @params = params
-  end
-
-  def show_gladiator
-    @movie = {
-      title: 'Gladiator',
-      video_id: 'FI1ylg4GKv8',
-      description: "Crowe shows everyone what's up."
-    }
-  end
-
-  def show_matilda
-    @movie = {
-      title: 'Matilda',
-      video_id: 'EVWOQwZENBg',
-      description: "Quite the random movie, but a classic for sure!"
-    }
     
   end
 
-  def show_seinfeld
-    @movie = {
-      title: 'Seinfeld',
-      video_id: 'm5OoR0Qjrlo',
-      description: 'A great description on where socks really go'
-    }
-  end
+  def show
+    video_name = params["video_name"]
 
-  def show_friends
-    @movie = {
-      title: 'Friends',
-      video_id: 'XIsVHF2bwVs',
-      description: 'Gotta love friends!'
-    }
-  end
+    videos = {
+      "gladiator" => {
+        title: 'Gladiator',
+        video_id: 'FI1ylg4GKv8',
+        description: "Crowe shows everyone what's up."        
+      },
 
+      "matilda" => {
+        title: 'Matilda',
+        video_id: 'EVWOQwZENBg',
+        description: "Quite the random movie, but a classic for sure!"
+      },
+
+      "seinfeld" => {
+        title: 'Seinfeld',
+        video_id: 'm5OoR0Qjrlo',
+        description: 'A great description on where socks really go'
+        },
+
+      "friends" => {
+        title: 'Friends',
+        video_id: 'XIsVHF2bwVs',
+        description: 'Gotta love friends!'
+      }
+    }
+
+    @video = videos[video_name]
+  end
+    
 end
+
+
+
+# def show_gladiator
+#     @movie = {
+#       title: 'Gladiator',
+#       video_id: 'FI1ylg4GKv8',
+#       description: "Crowe shows everyone what's up."
+#     }
+#   end
+
+#   def show_matilda
+#     @movie = {
+#       title: 'Matilda',
+#       video_id: 'EVWOQwZENBg',
+#       description: "Quite the random movie, but a classic for sure!"
+#     }
+    
+#   end
+
+
+# def show_seinfeld
+#     @movie = {
+#       title: 'Seinfeld',
+#       video_id: 'm5OoR0Qjrlo',
+#       description: 'A great description on where socks really go'
+#     }
+#   end
+
+#   def show_friends
+#     @movie = {
+#       title: 'Friends',
+#       video_id: 'XIsVHF2bwVs',
+#       description: 'Gotta love friends!'
+#     }
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 MetubeCohort3::Application.routes.draw do
 
-	get "/videos/:video_name" => 'videos#show', as: 'video'
 	root "videos#show_all"
-  get '/videos' => 'videos#show_all'
-  # get '/videos/gladiator' => 'videos#show_gladiator', as: 'gladiator_video'
+	get '/videos' => 'videos#show_all', as: 'videos'
+	get "/videos/:video_name" => 'videos#show', as: 'video'
+	
+end
+
+
+ 	# get '/videos/gladiator' => 'videos#show_gladiator', as: 'gladiator_video'
   # get '/videos/matilda' => 'videos#show_matilda', as: 'matilda_video'  
   # get '/videos/seinfeld' => 'videos#show_seinfeld', as: 'seinfeld_video'  
   # get '/videos/friends' => 'videos#show_friends', as: 'friends_video'
-
-end
